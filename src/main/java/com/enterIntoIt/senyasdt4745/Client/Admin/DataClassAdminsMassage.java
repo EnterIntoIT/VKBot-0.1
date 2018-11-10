240347933f1d2430c72740271c7db5ca60cefdc1
Arseny <senyasdt4745@gmail.com>
2018-11-11 01:21:57 +0300

    New Commit

---
+++ src/main/java/com/enterIntoIt/senyasdt4745/Client/Admin/DataClassAdminsMassage.java
@@ -0,0 +1,8 @@
+package com.enterIntoIt.senyasdt4745.Client.Admin;
+
+public class DataClassAdminsMassage {
+    public String pin;
+    public String time;
+    public String text;
+    public String attachments;
+}
