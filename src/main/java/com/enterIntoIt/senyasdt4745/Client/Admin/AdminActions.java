aacce6d3661ac902c410aa67920c2930a3b6118a
Arseny <senyasdt4745@gmail.com>
2018-11-11 00:49:31 +0300

    added all files 2

--- src/main/java/com/enterIntoIt/senyasdt4745/Client/Admin/AdminActions.java
+++ src/main/java/com/enterIntoIt/senyasdt4745/Client/Admin/AdminActions.java
@@ -1,4 +1,8 @@
 package com.enterIntoIt.senyasdt4745.Client.Admin;
 
+import com.enterIntoIt.*;
+import com.vk.api.sdk.*;
+
 public class AdminActions {
+
 }
---
+++ src/main/kotlin/com/enterIntoIt/pupptmst/NewPost.kt
@@ -0,0 +1,2 @@
+package com.enterIntoIt.pupptmst
+
