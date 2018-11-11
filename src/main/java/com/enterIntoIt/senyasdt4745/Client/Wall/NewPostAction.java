08facd336b871e77f6c8bee6abd2b7bea3590f1a
Arseny <senyasdt4745@gmail.com>
2018-11-11 08:51:09 +0300

    New commit when you sleep

--- src/main/java/com/enterIntoIt/senyasdt4745/Client/Wall/NewPostAction.java
+++ src/main/java/com/enterIntoIt/senyasdt4745/Client/Wall/NewPostAction.java
@@ -5,7 +5,9 @@ import com.enterIntoIt.senyasdt4745.*;
 import com.enterIntoIt.senyasdt4745.Client.Actor.BotActor;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.objects.apps.responses.GetResponse;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.wall.responses.PostResponse;
 i
 
 public class NewPostAction {
@@ -14,9 +16,15 @@ public class NewPostAction {
     UserActor actor = new UserActor(groupId, "qw");
 
     public void sendPostToWall(){
-        GetResponse getResponse = .wall().post(actor)
-                .attachments("00")
-                .execute();
+        try {
+            PostResponse getResponse = BotActor.client.wall().post(actor)
+                    .attachments("00")
+                    .execute();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
         //client.wall().post((UserActor) BotActor.actor);
     }
 
