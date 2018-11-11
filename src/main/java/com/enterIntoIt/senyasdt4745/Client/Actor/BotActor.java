3b06f789255cd27c2d58038eccc1f9d00d7fe37e
payforplayonsteam@yandex.ru <payforplayonsteam@yandex.ru>
2018-11-11 08:33:06 +0300

    fix actor

--- src/main/java/com/enterIntoIt/senyasdt4745/Client/Actor/BotActor.java
+++ src/main/java/com/enterIntoIt/senyasdt4745/Client/Actor/BotActor.java
@@ -1,8 +1,17 @@
 package com.enterIntoIt.senyasdt4745.Client.Actor;
 
 import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.client.actors.UserActor;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class BotActor {
 
-    GroupActor actor = new GroupActor();
+    BufferedReader inpFile = new BufferedReader(
+            new InputStreamReader(
+                    new FileInputStream("Keys.txt")));
+
+    UserActor actor = new GroupActor(171364230, inpFile.readLine() );
 }
