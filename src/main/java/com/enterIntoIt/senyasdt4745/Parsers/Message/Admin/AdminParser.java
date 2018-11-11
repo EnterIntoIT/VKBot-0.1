69f4b4e93eef348ff61892a2141bf0aa6f36b548
Arseny <senyasdt4745@gmail.com>
2018-11-11 07:05:01 +0300

    New commit when you sleep

--- src/main/java/com/enterIntoIt/senyasdt4745/Parsers/Message/Admin/AdminParser.java
+++ src/main/java/com/enterIntoIt/senyasdt4745/Parsers/Message/Admin/AdminParser.java
@@ -4,6 +4,7 @@ import com.enterIntoIt.senyasdt4745.Client.Admin.DataClassAdminsMassage;
 import com.vk.api.sdk.objects.messages.Message;
 import com.vk.api.sdk.objects.messages.MessageAttachment;
 
+import java.io.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,7 +14,7 @@ import java.util.*;
 public class AdminParser extends Message {
     static Message m = new Message();
     static Long unixTime;
-    public static DataClassAdminsMassage parseMessage() throws ParseException {
+    public static DataClassAdminsMassage parseMessage() throws ParseException, IOException {
         String parText = m.getBody();
         List<MessageAttachment> parAttachment = m.getAttachments();
         String [] parArray = parText.split("\n\r", 3);
@@ -21,10 +22,31 @@ public class AdminParser extends Message {
         * Первая строка сообщения - тип закрепления(p/u)(parArray[0])
         * Вторая строка - время в нужном формате(написан ниже)(автоматически переводится в unixTime)
         * Остальное - текст поста(parArray[2])
+        *
         * */
+        if(!ifAdmin()){
+            return null;
+        }
         AdminParser.changeDate(parArray[1]);
 
         return new DataClassAdminsMassage(parArray[0], unixTime, parArray[2], parAttachment);
+    }
+
+    static boolean ifAdmin() throws IOException {
+        BufferedReader inpFile = new BufferedReader(
+                new InputStreamReader(
+                new FileInputStream("/home/arseny/VK 0.1/VKBot-0.1/src/main/java/com/enterIntoIt/AdminID.txt")));
+        String [] adminsId = inpFile.readLine().split(" ");
+        for (String adminId: adminsId
+             ) {
+            if(m.getUserId() == Integer.parseInt(adminId)){
+                inpFile.close();
+                return true;
+            }
+        }
+        inpFile.close();
+        return false;
+
     }
 
     static void changeDate(String time) throws ParseException {
--- src/main/java/com/enterIntoIt/senyasdt4745/Parsers/Message/MainParser.java
+++ src/main/java/com/enterIntoIt/senyasdt4745/Parsers/Message/MainParser.java
@@ -1,6 +1,22 @@
 package com.enterIntoIt.senyasdt4745.Parsers.Message;
 
+import com.enterIntoIt.senyasdt4745.Client.User.UserMassageHello;
+import com.vk.api.sdk.actions.Groups;
+import com.vk.api.sdk.callback.objects.group.CallbackGroupJoin;
+import com.vk.api.sdk.callback.objects.messages.CallbackMessage;
+import com.vk.api.sdk.callback.objects.messages.CallbackMessageType;
+import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.objects.messages.Action;
+import org.graalvm.compiler.core.common.GraalOptions;
 
-public class MainParser extends {
+public class MainParser {
+
+    CallbackGroupJoin joinToGroup = new CallbackGroupJoin();
+
+    public void redirectToAction() {
+        if(joinToGroup.getJoinType().getValue().equals("join")){
+            UserMassageHello.massage();
+        }  //if (CallbackMessageType = ) {
+        //}
+    }
 }
